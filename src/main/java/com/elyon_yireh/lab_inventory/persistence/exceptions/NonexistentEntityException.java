7c794c41c0056cc482e564a8aad50810f77f1453
Danilo Arenas Yi <danilo.arenasyi@gmail.com>
2024-08-12 22:41:33 -0500

    Exceptions

---
+++ src/main/java/com/elyon_yireh/lab_inventory/persistence/exceptions/NonexistentEntityException.java
@@ -0,0 +1,10 @@
+package com.elyon_yireh.lab_inventory.persistence.exceptions;
+
+public class NonexistentEntityException extends Exception {
+    public NonexistentEntityException(String message, Throwable cause) {
+        super(message, cause);
+    }
+    public NonexistentEntityException(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/com/elyon_yireh/lab_inventory/persistence/exceptions/PreexistingEntityException.java
@@ -0,0 +1,10 @@
+package com.elyon_yireh.lab_inventory.persistence.exceptions;
+
+public class PreexistingEntityException extends Exception {
+    public PreexistingEntityException(String message, Throwable cause) {
+        super(message, cause);
+    }
+    public PreexistingEntityException(String message) {
+        super(message);
+    }
+}
